97dfae0232151a5db7b4bd2f028dccd2cdd700a3
ManishKrishnaSingh <30351569+ManishKrishnaSingh@users.noreply.github.com>
2022-02-19 21:23:27 +0530

    [matrix] print all matrix diagonal elements

---
+++ matrix/PrintMatrixDiagonalElements.cpp
@@ -0,0 +1,51 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+#define ROW 5
+#define COL 4
+
+void PrintMatrix(int matrix[ROW][COL]){
+    for(int i=0; i<ROW; i++){
+        for(int j=0; j<COL; j++){
+            cout<<matrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void PrintDiagonalOrder(int matrix[ROW][COL]){
+    vector<vector<int>> diagonal(ROW+COL-1);
+    for(int i=0; i<ROW; i++){
+        for(int j=0; j<COL; j++){
+            diagonal[i+j].push_back(matrix[i][j]);
+        }
+    }
+
+    for(int i=0; i<diagonal.size(); i++){
+        for(int j=diagonal[i].size()-1; j>=0; j--){
+            cout<<diagonal[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int main(){
+    int matrix[ROW][COL]={
+        { 1,   2,  3,  4 },
+        { 5,   6,  7,  8 },
+        { 9,  10, 11, 12 },
+        { 13, 14, 15, 16 },
+        { 17, 18, 19, 20 },
+    };
+
+    cout<<"Matrix:\n";
+    PrintMatrix(matrix);
+
+    cout<<"\nDiagonal:\n";
+    PrintDiagonalOrder(matrix);
+
+    return 0;
+}
+
