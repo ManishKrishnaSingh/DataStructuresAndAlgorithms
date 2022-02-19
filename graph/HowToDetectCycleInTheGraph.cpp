2fd9d4f552455e5842ae9fed4577ff70ea6a8860
ManishKrishnaSingh <30351569+ManishKrishnaSingh@users.noreply.github.com>
2022-02-19 23:54:24 +0530

    [graph] detect cycle in graph using union-find

---
+++ graph/HowToDetectCycleInTheGraph.cpp
@@ -0,0 +1,66 @@
+#include <list>
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+typedef pair<int,int> Edge;
+
+class Graph{
+  private:
+	int V;
+	list<Edge> listOfEdge;
+
+  public:
+	Graph(int V){
+		this->V = V;
+	}
+
+	void addEdge(int src, int dst){
+		listOfEdge.push_back({src, dst});
+	}
+
+	int find(int parent[], int index){
+	    if(parent[index] == -1){
+	        return index;
+	    }
+	    return find(parent, parent[index]);
+	}
+
+	void Union(int parent[], int u, int v){
+	    parent[u] = v;
+	}
+
+	bool hasCycle(){
+	    int parent[this->V];
+	    memset(parent, -1, sizeof(parent));
+
+	    list<Edge>::iterator it;
+	    for(it=listOfEdge.begin(); it!=listOfEdge.end(); it++){
+	        int x = find(parent, it->first);
+	        int y = find(parent, it->second);
+
+	        if(x==y){
+	            return true;
+	        }
+	        Union(parent, x, y);
+	    }
+	    return false;
+	}
+};
+
+
+
+int main(){
+	int V = 3;
+	Graph* graph = new Graph(V);
+
+	graph->addEdge(0, 1);
+	graph->addEdge(0, 2);
+	graph->addEdge(1, 2);
+
+	cout<<"Graph has Cycle = "<<boolalpha<<graph->hasCycle()<<endl;
+
+	return 0;
+}
+
