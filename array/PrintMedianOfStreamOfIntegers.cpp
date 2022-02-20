6de05a2cc602e1d7c505ff2cf949b851035bb608
Manish Singh <30351569+ManishKrishnaSingh@users.noreply.github.com>
2022-02-20 15:46:27 +0530

    Update PrintMedianOfStreamOfIntegers.cpp

--- array/PrintMedianOfStreamOfIntegers.cpp
+++ array/PrintMedianOfStreamOfIntegers.cpp
@@ -2,49 +2,58 @@
 
 using namespace std;
 
-void printMedians(int A[], int N){
+void medianOfStream(int arr[], int n){
     priority_queue<int> maxHeap;
-    priority_queue<int, vector<int>, greater<int>> minHeap;
+    priority_queue<int, vector<int>, greater<int> > minHeap;
+    
+    int median = arr[0];
+    maxHeap.push(arr[0]);
+    cout<<"Median: "<<median;
 
-    int median = 0;
-    for (int i=0; i < N; i++){
-        int num = A[i];
-        if (maxHeap.size() > minHeap.size()){
-            if (num < median){
-                minHeap.push(maxHeap.top());
-                maxHeap.pop();
-                maxHeap.push(num);
-            } else {
-                minHeap.push(num);
-            }
-            median = (maxHeap.top() + minHeap.top())/2;
-        } else if (maxHeap.size() == minHeap.size()){
-            if (num < median){
-                maxHeap.push(num);
+    for (int i=1; i < n; i++){
+        int key = arr[i];
+        if (maxHeap.size()==minHeap.size()){
+            if(key < median){
+                maxHeap.push(key);
                 median = maxHeap.top();
             } else {
-                minHeap.push(num);
+                minHeap.push(key);
                 median = minHeap.top();
             }
+        } else if (maxHeap.size() > minHeap.size()){
+            if(key < median){
+                minHeap.push(maxHeap.top());
+                maxHeap.pop();
+                maxHeap.push(key);
+            } else {
+                minHeap.push(key);
+            }
+            median = (maxHeap.top() + minHeap.top())/2.0;
         } else if (maxHeap.size() < minHeap.size()){
-            if (num > median){
+            if (key > median){
                 maxHeap.push(minHeap.top());
                 minHeap.pop();
-                minHeap.push(num);
+                minHeap.push(key);
             } else {
-                maxHeap.push(num);
+                maxHeap.push(key);
             }
-            median = (maxHeap.top() + minHeap.top())/2;
+            median = (maxHeap.top() + minHeap.top())/2.0;
         }
-        cout << median << " ";
+        cout<<" "<<median;
     }
+    cout<<endl;
 }
 
 int main(){
-    int A[] = {5, 15, 1, 3, 2, 8, 7, 9, 10, 6, 11, 4};
-    int N = sizeof(A)/sizeof(A[0]);
+    int arr1[] = {5, 15, 10, 20, 3};
+    int n1 = sizeof(arr1)/sizeof(arr1[0]);
 
-    printMedians(A, N);
+    medianOfStream(arr1, n1);
+
+    int arr2[] = {5, 15, 1, 3, 2, 8, 7, 9, 10, 6, 11, 4};
+    int n2 = sizeof(arr2)/sizeof(arr2[0]);
+
+    medianOfStream(arr2, n2);
+
     return 0;
 }
-
