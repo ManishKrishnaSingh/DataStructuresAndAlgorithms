c9f1a568de4383c3d8ae1e44bf072f614aa3ddd8
ManishKrishnaSingh <30351569+ManishKrishnaSingh@users.noreply.github.com>
2022-03-31 00:02:32 +0530

    [number] find count of possible decodings of digit sequence

---
+++ number/FindPossibleDecodingsOfDigitSequence.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int findPossibleWaysOfDecoding(string& digits){
+    int n=digits.size();
+
+    //"0123"->return 0
+    if(digits[0]=='0'){
+        return 0;
+    }
+
+    int dp[n+1];
+    memset(dp, 0, sizeof(dp));
+
+    dp[0] = dp[1] = 1;
+    for(int i=2; i<=n; i++){
+        if (digits[i-1] > '0'){
+            dp[i] = dp[i-1];
+        }
+        if (digits[i-2]=='1' || (digits[i-2]=='2' && digits[i-1]<'7')){
+            dp[i] += dp[i-2];
+        }
+    }
+
+    return dp[n];
+}
+
+int main(){
+    string digits="121325";
+    int nWays=findPossibleWaysOfDecoding(digits);
+    cout<<"Possible Ways Of Decoding = "<<nWays;
+    return 0;
+}
+
