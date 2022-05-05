dc54c8914c80e64f3177fd2f825c01c65d48fb42
Manish Singh <30351569+ManishKrishnaSingh@users.noreply.github.com>
2022-05-05 20:09:38 +0530

    [string] regular expressions match using dynamic programming

--- string/RegularExpressionMatchUsingDP.cpp
+++ string/RegularExpressionMatchUsingDP.cpp
@@ -6,22 +6,22 @@ using namespace std;
 /** Regular Expression Match: DP (Top-Down)  **/
 /**********************************************/
 bool matchesTopDown(string txt, string pat){
-    int tLen = txt.length();
-    int pLen = pat.length();
+    int m = txt.length();
+    int n = pat.length();
 
-    bool **dp = new bool*[tLen+1];
-    for(int i=0; i<=tLen; i++){
-        dp[i] = new bool [pLen+1];
-        for(int j=0; j<=pLen; j++){
+    bool **dp = new bool*[m+1];
+    for(int i=0; i<=m; i++){
+        dp[i] = new bool [n+1];
+        for(int j=0; j<=n; j++){
             dp[i][j] = false;
         }
     }
 
-    dp[tLen][pLen] = true;
-    for(int i=tLen; i>=0; i--){
-        for(int j=pLen-1; j>=0; j--){
-            bool IsMatch = (i<tLen && (pat[j]==txt[i] || pat[j]=='.'));
-            if(j+1 < pLen && pat[j+1]=='*'){
+    dp[m][n] = true;
+    for(int i=m; i>=0; i--){
+        for(int j=n-1; j>=0; j--){
+            bool IsMatch = (i<m && (pat[j]==txt[i] || pat[j]=='.'));
+            if(j+1 < n && pat[j+1]=='*'){
                 dp[i][j] = dp[i][j+2] || (IsMatch && dp[i+1][j]);
             } else {
                 dp[i][j] = (IsMatch && dp[i+1][j+1]);
@@ -36,26 +36,26 @@ bool matchesTopDown(string txt, string pat){
 /** Regular Expression Match: DP (Bottom-Up) **/
 /**********************************************/
 bool matchesBottomUp(string txt, string pat){
-    int tLen = txt.length();
-    int pLen = pat.length();
+    int m = txt.length();
+    int n = pat.length();
 
-    bool **dp = new bool*[tLen+1];
-    for(int i=0; i<=tLen; i++){
-        dp[i] = new bool [pLen+1];
-        for(int j=0; j<=pLen; j++){
+    bool **dp = new bool*[m+1];
+    for(int i=0; i<=m; i++){
+        dp[i] = new bool [n+1];
+        for(int j=0; j<=n; j++){
             dp[i][j] = false;
         }
     }
 
     dp[0][0] = true;
-    for(int i=1; i<pLen; i++){
+    for(int i=1; i<=n; i++){
         if(pat[i-1] == '*'){
             dp[0][i] = dp[0][i-2];
         }
     }
 
-    for(int i=1; i<=tLen; i++){
-        for(int j=1; j<=pLen; j++){
+    for(int i=1; i<=m; i++){
+        for(int j=1; j<=n; j++){
             if(pat[j-1]=='.' || pat[j-1]==txt[i-1]){
                 dp[i][j] = dp[i-1][j-1];
             } else if(pat[j-1]=='*'){
@@ -68,7 +68,7 @@ bool matchesBottomUp(string txt, string pat){
             }
         }
     }
-    return dp[tLen][pLen];
+    return dp[m][n];
 }
 
 int main() {
@@ -76,4 +76,3 @@ int main() {
     cout<<"\nIs Match[DP-BU]: "<<boolalpha<<matchesBottomUp("axxe", "ax*e");
     return 0;
 }
-
