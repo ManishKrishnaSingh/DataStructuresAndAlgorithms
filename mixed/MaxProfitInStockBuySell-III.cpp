0db94715a71bb4b831ffa864af3349b0ca38fa36
ManishKrishnaSingh <30351569+ManishKrishnaSingh@users.noreply.github.com>
2022-10-21 01:48:48 +0530

    [mixed] update logic of maximum profit in stock buy and sell-III

--- mixed/MaxProfitInStockBuySell-III.cpp
+++ mixed/MaxProfitInStockBuySell-III.cpp
@@ -8,26 +8,35 @@ Find maximum profit you can achieve. You may complete at most k transaction
 
 Note: You may not engage in multiple transactions simultaneously (i.e., you
 must sell the stock before you buy again).
+
+dp[T][S] = max(
+    dp[T][S-1],
+    max((price[S]–price[B]) + dp[T-1][B])   - for all B in range [0, S-1]
+)
 **************************************************************************/
 
 int maxProfitStockBuySell(int price[], int n, int k){
-    int profit[k+1][n+1];
+    int profit[k+1][n];
 
     //set profit to 0 if day 0
     for(int i=0; i <= k; i++){
         profit[i][0] = 0;
     }
     //set profit to 0 if k == 0
-    for(int j=0; j <= n; j++){
+    for(int j=0; j < n; j++){
         profit[0][j] = 0;
     }
 
+    int currMax;
     //update profit bottom-up
     for(int i=1; i<=k; i++){
-        int prevProfit = INT_MIN;
-        for (int j=1; j<n; j++) {
-            prevProfit = max(prevProfit, profit[i-1][j-1]-price[j-1]);
-            profit[i][j] = max(profit[i][j-1], price[j] + prevProfit);
+        for(int j=1; j<n; j++){
+            currMax = INT_MIN;
+            for(int m=0; m < j; m++){
+                currMax = max(currMax, price[j]-price[m] + profit[i-1][m]);
+            }
+            // max of with sell and without sell at j
+            profit[i][j] = max(profit[i][j-1], currMax);
         }
     }
 
@@ -40,7 +49,7 @@ int main(){
     int n = sizeof(price) / sizeof(price[0]);
 
     int maxProfit = maxProfitStockBuySell(price, n, k);
-    cout<<"Max Profit[Stock Buy & Sell]="<<maxProfit;
+    cout<<"Max Profit[Stock Buy & Sell] = "<<maxProfit;
 
     return 0;
 }
