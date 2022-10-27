2fa691d62f086d2f555cc22f999c59637ce0154a
ManishKrishnaSingh <30351569+ManishKrishnaSingh@users.noreply.github.com>
2022-10-27 22:50:59 +0530

    [string] find longest common sub-sequence

--- string/LongestCommonSubSequence.cpp
+++ string/LongestCommonSubSequence.cpp
@@ -2,16 +2,16 @@
 
 using namespace std;
 
-int findLongestCommonSubSequence(string iStr1, string iStr2){
-    int m = iStr1.length();
-    int n = iStr2.length();
+int findLongestCommonSubSequenceLength(string xStr, string yStr){
+    int m = xStr.length();
+    int n = yStr.length();
 
     int dp[m+1][n+1];
     memset(dp, 0, sizeof(dp));
 
     for(int i=1; i<=m; i++){
         for(int j=1; j<=n; j++){
-            if(iStr1[i-1] == iStr2[j-1]){
+            if(xStr[i-1] == yStr[j-1]){
                 dp[i][j] = 1 + dp[i-1][j-1];
             } else {
                 dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
@@ -22,13 +22,49 @@ int findLongestCommonSubSequence(string iStr1, string iStr2){
     return dp[m][n];
 }
 
+string findLongestCommonSubSequenceString(string xStr, string yStr){
+    int m = xStr.length();
+    int n = yStr.length();
+
+    int dp[m+1][n+1];
+    memset(dp, 0, sizeof(dp));
+
+    for(int i=1; i<=m; i++){
+        for(int j=1; j<=n; j++){
+            if(xStr[i-1] == yStr[j-1]){
+                dp[i][j] = 1 + dp[i-1][j-1];
+            } else {
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+    }
+
+    string str = "";
+
+    int i = m, j = n;
+    while(i > 0 and j > 0){
+        if(xStr[i-1] == yStr[j-1]){
+            str = xStr[i-1] + str;
+            i--; j--;
+        } else if(dp[i-1][j] > dp[i][j-1]){
+            i--;
+        } else {
+            j--;
+        }
+    }
+
+    return str;
+}
+
 int main(){
     string str1 = "AGGTAB";
     string str2 = "GXTXAYB";
 
-    int length=findLongestCommonSubSequence(str1, str2);
-    cout<<"Longest Common Sub Sequence = "<<length<<endl;
+    int ssLength = findLongestCommonSubSequenceLength(str1, str2);
+    cout<<"Longest Common Sub Sequence = "<<ssLength<<endl;
+
+    string ssStr = findLongestCommonSubSequenceString(str1, str2);
+    cout<<"Longest Common Sub Sequence = "<<ssStr<<endl;
 
     return 0;
 }
-
