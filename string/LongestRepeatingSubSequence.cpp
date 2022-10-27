28c23d6dd62d141f354b430a56ba7622b40028db
ManishKrishnaSingh <30351569+ManishKrishnaSingh@users.noreply.github.com>
2022-10-27 23:39:04 +0530

    [string] find longest repeating sub-sequence

--- string/LongestRepeatingSubSequence.cpp
+++ string/LongestRepeatingSubSequence.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int findLongestRepeatingSubSeq(string iStr){
+int findLongestRepeatingSubSequenceLength(string iStr){
     int n = iStr.length();
 
     int dp[n+1][n+1];
@@ -10,7 +10,7 @@ int findLongestRepeatingSubSeq(string iStr){
 
     for(int i=1; i<=n; i++){
         for(int j=1; j<=n; j++){
-            if(i!=j && iStr[i-1] == iStr[j-1]){
+            if(i != j and iStr[i-1] == iStr[j-1]){
                 dp[i][j] = 1 + dp[i-1][j-1];
             } else {
                 dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
@@ -21,12 +21,47 @@ int findLongestRepeatingSubSeq(string iStr){
     return dp[n][n];
 }
 
-int main(){
-    string str = "aabb";
+string findLongestRepeatingSubSequenceString(string iStr){
+    int n = iStr.length();
 
-    int length = findLongestRepeatingSubSeq(str);
-    cout<<"Longest Repeating SubSeq = "<<length<<endl;
+    int dp[n+1][n+1];
+    memset(dp, 0, sizeof(dp));
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            if(i != j and iStr[i-1] == iStr[j-1]){
+                dp[i][j] = 1 + dp[i-1][j-1];
+            } else {
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+    }
+
+    string str = "";
+
+    int i = n, j = n;
+    while(i > 0 and j > 0){
+        if(i != j and iStr[i-1] == iStr[j-1]){
+            str = iStr[i-1] + str;
+            i--; j--;
+        } else if(dp[i-1][j] > dp[i][j-1]){
+            i--;
+        } else {
+            j--;
+        }
+    }
+
+    return str;
+}
+
+int main(){
+    string str = "aabfbcdd";
+
+    int ssLength = findLongestRepeatingSubSequenceLength(str);
+    cout<<"Longest Repeating Sub Sequence = "<<ssLength<<endl;
+
+    string ssStr = findLongestRepeatingSubSequenceString(str);
+    cout<<"Longest Repeating Sub Sequence = "<<ssStr<<endl;
 
     return 0;
 }
-
