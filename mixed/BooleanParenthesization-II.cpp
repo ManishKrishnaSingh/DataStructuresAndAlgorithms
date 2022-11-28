a51fc00e5f657f12950c48c7d36affaf818c3465
ManishKrishnaSingh <30351569+ManishKrishnaSingh@users.noreply.github.com>
2022-11-28 22:07:02 +0530

    [mixed] no of ways to put parenthesis to keep it to true - dp

--- mixed/BooleanParenthesization-II.cpp
+++ mixed/BooleanParenthesization-II.cpp
@@ -5,73 +5,73 @@ using namespace std;
 int dp[10][10][2];
 
 int countWays(string str, int i, int j, int isTrue){
-    if(i>j){
+    if(i > j){
         return 0;
     }
 
-    if(i==j){
+    if(i == j){
         if(isTrue){
-            return str[i]=='T';
+            return str[i] == 'T';
         } else {
-            return str[i]=='F';
+            return str[i] == 'F';
         }
     }
 
-    if(dp[i][j][isTrue]!=-1){
+    if(dp[i][j][isTrue] != -1){
         return dp[i][j][isTrue];
     }
 
-    int result = 0;
-    for(int k=i+1;k<=j-1;k=k+2){
+    int counter = 0;
+    for(int k=i+1; k<=j-1; k=k+2){
         int leftT,leftF,rightT,rightF;
 
-        if(dp[i][k-1][1]==-1){
+        if(dp[i][k-1][1] == -1){
             leftT = countWays(str,i,k-1,1);
         } else {
             leftT = dp[i][k-1][1];
         }
 
-        if(dp[i][k-1][0]==-1){
+        if(dp[i][k-1][0] == -1){
             leftF = countWays(str,i,k-1,0);
         } else {
             leftF = dp[i][k-1][0];
         }
 
-        if(dp[k+1][j][1]==-1){
+        if(dp[k+1][j][1] == -1){
             rightT = countWays(str,k+1,j,1);
         } else {
             rightT = dp[k+1][j][1];
         }
 
-        if(dp[k+1][j][0]==-1){
+        if(dp[k+1][j][0] == -1){
             rightF = countWays(str,k+1,j,0);
         } else {
             rightF = dp[k+1][j][0];
         }
 
-        if(str[k]=='^'){
+        if(str[k] == '^'){
             if(isTrue){
-                result += (leftT*rightF) + (leftF*rightT);
+                counter += (leftT*rightF) + (leftF*rightT);
             } else {
-                result += (leftT*rightT) + (leftF*rightF);
+                counter += (leftT*rightT) + (leftF*rightF);
             }
-        } else if(str[k]=='&'){
+        } else if(str[k] == '&'){
             if(isTrue){
-                result += (leftT*rightT);
+                counter += (leftT*rightT);
             } else {
-                result += (leftT*rightF) + (leftF*rightT) + (leftF*rightF);
+                counter += (leftT*rightF) + (leftF*rightT) + (leftF*rightF);
             }
-        } else if(str[k]=='|'){
+        } else if(str[k] == '|'){
             if(isTrue){
-                result += (leftT*rightF) + (leftF*rightT) + (leftT*rightT);
+                counter += (leftT*rightF) + (leftF*rightT) + (leftT*rightT);
             } else {
-                result += (leftF*rightF);
+                counter += (leftF*rightF);
             }
         }
 
-        dp[i][j][isTrue] = result%1003;
+        dp[i][j][isTrue] = counter%1003;
     }
-    return result%1003;
+    return counter%1003;
 }
 
 int numOfWays(string symbols, string operators){
