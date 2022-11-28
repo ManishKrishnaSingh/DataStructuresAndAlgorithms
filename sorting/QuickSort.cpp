2e603d2fe01ac84c00bea7a15760c41cd519a9a8
ManishKrishnaSingh <30351569+ManishKrishnaSingh@users.noreply.github.com>
2022-11-29 03:18:03 +0530

    [sorting] implement quick sort

--- sorting/QuickSort.cpp
+++ sorting/QuickSort.cpp
@@ -5,15 +5,15 @@ using namespace std;
 int partition(int arr[], int low, int high){
     int pivot = arr[high];
 
-    int i=low-1;
+    int idx = low;
     for(int j=low; j<high; j++){
         if (arr[j] < pivot) {
-            i++;
-            swap(arr[i], arr[j]);
+            swap(arr[idx++], arr[j]);
         }
     }
-    swap(arr[i+1], arr[high]);
-    return i+1;
+    swap(arr[idx], arr[high]);
+
+    return idx;
 }
 
 void quickSortArray(int arr[], int low, int high){
