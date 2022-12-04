50b625521e5f89fc38de06439a4efe2bf5742cba
ManishKrishnaSingh <30351569+ManishKrishnaSingh@users.noreply.github.com>
2022-12-04 18:57:21 +0530

    [trees] find min in the range using segment tree

--- trees/SegmentTree/RangeMinSegmentTree.cpp
+++ trees/SegmentTree/RangeMinSegmentTree.cpp
@@ -1,19 +1,17 @@
-#include <cmath>
-#include <climits>
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-void updateSegmentTreeUtil(int *sTree, int aBeg, int aEnd, int i, int diff, int idx){
-    if (i < aBeg || i > aEnd){
+void updateSegmentTreeUtil(int *sTree, int low, int high, int i, int diff, int idx){
+    if (i < low || i > high){
         return;
     }
 
     sTree[idx] = sTree[idx] + diff;
-    if (aEnd != aBeg){
-        int mid = (aBeg+aEnd) >> 1;
-        updateSegmentTreeUtil(sTree, aBeg, mid, i, diff, 2*idx + 1);
-        updateSegmentTreeUtil(sTree, mid+1, aEnd, i, diff, 2*idx + 2);
+    if (low != high){
+        int mid = (low+high) >> 1;
+        updateSegmentTreeUtil(sTree, low, mid, i, diff, 2*idx + 1);
+        updateSegmentTreeUtil(sTree, mid+1, high, i, diff, 2*idx + 2);
     }
 }
 
@@ -28,17 +26,20 @@ void updateSegmentTree(int arr[], int *sTree, int n, int idx, int newValue){
     updateSegmentTreeUtil(sTree, 0, n-1, idx, diff, 0);
 }
 
-int minRangeUtil(int* sTree, int aBeg, int aEnd, int qBeg, int qEnd, int idx){
-    if(qBeg<=aBeg && qEnd>=aEnd){
+int minRangeUtil(int* sTree, int low, int high, int qBeg, int qEnd, int idx){
+    if(qBeg<=low && qEnd>=high){
         return sTree[idx];
     }
 
-    if(aEnd < qBeg || aBeg > qEnd){
+    if(high < qBeg || low > qEnd){
         return INT_MAX;
     }
 
-    int mid = (aBeg+aEnd) >> 1;
-    return min(minRangeUtil(sTree,aBeg,mid,qBeg,qEnd,2*idx+1), minRangeUtil(sTree,mid+1,aEnd,qBeg,qEnd,2*idx+2));
+    int mid = (low+high) >> 1;
+    return min(
+        minRangeUtil(sTree, low, mid, qBeg, qEnd, 2*idx+1),
+        minRangeUtil(sTree, mid+1, high, qBeg, qEnd, 2*idx+2)
+    );
 }
 
 int minRange(int* sTree, int n, int qBeg, int qEnd){
@@ -49,22 +50,25 @@ int minRange(int* sTree, int n, int qBeg, int qEnd){
 }
 
 
-int createSegmentTreeUtil(int arr[], int aBeg, int aEnd, int* sTree, int idx){
-    if(aBeg == aEnd){
-        sTree[idx] = arr[aBeg];
+int buildSegmentTreeUtil(int arr[], int low, int high, int* sTree, int idx){
+    if(low == high){
+        sTree[idx] = arr[low];
     } else {
-        int mid = (aBeg+aEnd) >> 1;
-        sTree[idx] = min(createSegmentTreeUtil(arr,aBeg,mid,sTree,2*idx+1), createSegmentTreeUtil(arr,mid+1,aEnd,sTree,2*idx+2));
+        int mid = (low+high) >> 1;
+        sTree[idx] = min(
+            buildSegmentTreeUtil(arr, low, mid, sTree, 2*idx+1),
+            buildSegmentTreeUtil(arr, mid+1, high, sTree, 2*idx+2)
+        );
     }
     return sTree[idx];
 }
 
-int* createSegmentTree(int arr[], int n){
+int* buildSegmentTree(int arr[], int n){
     int x = ceil(log2(n));
     int size = 2*pow(2,x) - 1;
 
     int* sTree = new int[size];
-    createSegmentTreeUtil(arr, 0, n-1, sTree, 0);
+    buildSegmentTreeUtil(arr, 0, n-1, sTree, 0);
 
     return sTree;
 }
@@ -73,7 +77,7 @@ int main(){
     int arr[] = {1, 3, 5, 7, 9, 11};
     int n = sizeof(arr)/sizeof(arr[0]);
 
-    int *sTree = createSegmentTree(arr, n);
+    int *sTree = buildSegmentTree(arr, n);
     cout<<"ST MIN [3-5] = "<<minRange(sTree,n,1,3)<<endl;
 
     updateSegmentTree(arr, sTree, n, 1, 10);
@@ -81,4 +85,3 @@ int main(){
 
     return 0;
 }
-
