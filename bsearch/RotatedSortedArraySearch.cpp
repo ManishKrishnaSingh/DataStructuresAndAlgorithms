85a4b0004e7b802b56dd5ec4abd8b181223c6cc0
Manish Singh <singhmanish@microsoft.com>
2023-04-15 17:26:53 +0530

    [bsearch] rotated sorted array search

--- bsearch/RotatedSortedArraySearch.cpp
+++ bsearch/RotatedSortedArraySearch.cpp
@@ -1,44 +1,107 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int search(int arr[], int low, int high, int key){
-    int mid = (low+high) >> 1;
-    if(arr[mid] == key){
-        return mid;
-    }
+int search(vector<int>& nums, int key)
+{
+    if(!nums.empty())
+    {
+        int n = nums.size();
+        int left=0, right=n-1;
 
-    // this case is added for low+1 == high
-    if(low==mid && (arr[mid]!=key || arr[mid+1]!=key)){
-        return -1;
-    }
+        int mid;
+        while(left <= right)
+        {
+            mid = (left+right) >> 1;
+            if(nums[mid] == key)
+            {
+                return mid;
+            }
 
-    if(arr[low] < arr[mid]) {
-        // left side sorted
-        if(key < arr[mid]){
-            return search(arr, low, mid, key);
-        } else {
-            return search(arr, mid, high, key);
-        }
-    } else {
-        // right side sorted
-        if(key > arr[mid]){
-            return search(arr, mid, high, key);
-        } else {
-            return search(arr, low, mid, key);
+            if(nums[left] <= nums[mid]) // left side sorted
+            {
+                if(key < nums[mid] and key >= nums[left])
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid + 1;
+                }
+            }
+            else // right side sorted
+            {
+                if(key > nums[mid] and key <= nums[right])
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
         }
     }
 
     return -1;
 }
 
-int main(){
-    int input[] = { 4, 5, 6, 7, 8, 9, 1, 2 };
-    int n = sizeof(input) / sizeof(input[0]);
+int searchRecursive(vector<int>& nums, int low, int high, int key)
+{
+    int mid = (low+high) >> 1;
+    if(nums[mid] == key)
+    {
+        return mid;
+    }
 
-    int key = 7;
-    int index = search(input, 0, n-1, key);
+    // this case is added for low+1 == high
+    if(low==mid && (nums[mid] != key || nums[mid+1] != key))
+    {
+        return -1;
+    }
 
+    if(nums[low] < nums[mid])
+    {
+        // left side sorted
+        if(key < nums[mid])
+        {
+            return searchRecursive(nums, low, mid, key);
+        }
+        else
+        {
+            return searchRecursive(nums, mid, high, key);
+        }
+    }
+    else
+    {
+        // right side sorted
+        if(key > nums[mid])
+        {
+            return searchRecursive(nums, mid, high, key);
+        }
+        else
+        {
+            return searchRecursive(nums, low, mid, key);
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+    vector<int> input = { 4, 5, 6, 7, 8, 9, 1, 2 };
+
+    int key = 7, index = -1, n = input.size();
+
+    index = search(input, key);
+    if (index != -1){
+        cout << "Key found at Index: " << index << endl;
+    } else {
+        cout << "Key is not found in the array!" << endl;
+    }
+
+    index = searchRecursive(input, 0, n-1, key);
     if (index != -1){
         cout << "Key found at Index: " << index << endl;
     } else {
@@ -47,4 +110,3 @@ int main(){
 
     return 0;
 }
-
