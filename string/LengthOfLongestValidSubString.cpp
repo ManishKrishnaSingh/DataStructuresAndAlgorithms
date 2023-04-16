eb896943fd49b9a10184fbaa49d588247b970b0f
Manish Singh <singhmanish@microsoft.com>
2023-04-17 00:17:36 +0530

    [string] find length of longest valid sub-string

--- string/LengthOfLongestValidSubString.cpp
+++ string/LengthOfLongestValidSubString.cpp
@@ -1,6 +1,4 @@
-#include <stack>
-#include <climits>
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -29,11 +27,60 @@ int findMaxLength(string str){
     return maxLen;
 }
 
+int maxLengthValidSubstr(string str)
+{
+    int n = str.length();
+    int index, left, right, maxLen=0;
+
+    left = right = 0;
+    // process from left to right
+    for(index=0; index < n; index++)
+    {
+        if(str[index] == '(')
+            left++;
+        else
+            right++;
+
+        if(left == right)
+        {
+            maxLen = max(maxLen, 2*right);
+        }
+        else if(right > left)
+        {
+            left = right = 0;
+        }
+    }
+
+    left = right = 0;
+    // process from right to left
+    for(index=n-1; index >= 0; index--)
+    {
+        if(str[index] == '(')
+            left++;
+        else
+            right++;
+
+        if(left == right)
+        {
+            maxLen = max(maxLen, 2*left);
+        }
+        else if(left > right)
+        {
+            left = right = 0;
+        }
+    }
+
+    return maxLen;
+}
+
 int main(){
     string str = "())(()(()))))";
 
     int maxLength = findMaxLength(str);
-    cout<<"Max Length = "<<maxLength<<endl;
+    cout<<"Max Length[1] = "<<maxLength;
+
+    int maxLen = maxLengthValidSubstr(str);
+    cout<<"\n\n\nMax Length[2] = "<<maxLen;
 
     return 0;
 }
