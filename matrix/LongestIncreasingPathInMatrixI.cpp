1a61f2f28194e737505a454f9c152364d3fe6b35
Manish Singh <30351569+ManishKrishnaSingh@users.noreply.github.com>
2023-06-28 23:53:39 +0530

    Update LongestIncreasingPathInMatrixI.cpp

--- matrix/LongestIncreasingPathInMatrixI.cpp
+++ matrix/LongestIncreasingPathInMatrixI.cpp
@@ -5,32 +5,23 @@ const int N = 4;
 
 using namespace std;
 
-int LipUtil(int dp[N+1][N+1], int mat[N][N], int x, int y){
-    if (dp[x][y] < 0)
-	{
+int dfs(int dp[N+1][N+1], int mat[N][N], int x, int y)
+{
+    if (dp[x][y] == -1)
+    {
+        int right = 0, down = 0;
 
-        if (x == N-1 && y == N-1)
-		{
-            return dp[x][y] = 1;
-        }
-
-        int result = 0;
-        if (x == N-1 || y == N-1)
-		{
-            result = 1;
-        }
-        
         if ((x+1) < N and mat[x][y] < mat[x + 1][y])
-		{
-            result = max(result, 1 + LipUtil(dp, mat, x + 1, y));
+	{
+            right = dfs(dp, mat, x + 1, y);
         }
         
         if ((y+1) < N and mat[x][y] < mat[x][y + 1])
-		{
-            result = max(result, 1 + LipUtil(dp, mat, x, y + 1));
+	{
+            down = dfs(dp, mat, x, y + 1);
         }
 
-        dp[x][y] = result;
+        dp[x][y] = 1 + max(right, down);
     }
     return dp[x][y];
 }
@@ -38,7 +29,7 @@ int LipUtil(int dp[N+1][N+1], int mat[N][N], int x, int y){
 int LIP(int mat[N][N]){
     int dp[N+1][N+1];
     memset(dp, -1, sizeof dp);
-    return LipUtil(dp, mat, 0, 0);
+    return dfs(dp, mat, 0, 0);
 }
 
 int main(){
