d8fcc5369db6b868dd79463c6d64e9a77704d900
ManishKrishnaSingh <30351569+ManishKrishnaSingh@users.noreply.github.com>
2023-07-12 02:14:06 +0530

    [mixed] add time n space complexities for string count under constraints

--- mixed/StringCountUnderConstraints.cpp
+++ mixed/StringCountUnderConstraints.cpp
@@ -2,57 +2,83 @@
 
 using namespace std;
 
-int findCount(int n, int bCount, int cCount){
-    if (bCount<0 || cCount<0){
+/********************************************************************
+Given a length n, count the number of strings of length n that can be
+made using 'a', 'b' and 'c' with at most one 'b' and two 'c's allowed
+*********************************************************************/
+
+int findCount(int n, int bCount, int cCount)
+{
+    if (bCount<0 || cCount<0)
+    {
         return 0;
     }
-    if (n==0 || (bCount==0 && cCount==0)){
+
+    if (n==0 || (bCount==0 && cCount==0))
+    {
         return 1;
     }
 
-    int res = 0;
-    res += findCount(n-1, bCount, cCount);
-    res += findCount(n-1, bCount-1, cCount);
-    res += findCount(n-1, bCount, cCount-1);
+    int counter = 0;
+    counter += findCount(n-1, bCount, cCount);
+    counter += findCount(n-1, bCount-1, cCount);
+    counter += findCount(n-1, bCount, cCount-1);
 
-    return res;
+    return counter;
 }
 
-int stringCountUtil(int dp[][2][3], int n, int bCount, int cCount){
-    if (bCount<0 || cCount<0){
+
+int dpFindCountUtil(int dp[][2][3], int n, int bCount, int cCount)
+{
+    if (bCount<0 || cCount<0)
+    {
         return 0;
     }
-    if (n==0 || (bCount==0 && cCount==0)){
+
+    if (n==0 || (bCount==0 && cCount==0))
+    {
         return 1;
     }
 
-    if(dp[n][bCount][cCount] != -1){
+    if(dp[n][bCount][cCount] != -1)
+    {
         return dp[n][bCount][cCount];
     }
 
-    int res = 0;
-    res += stringCountUtil(dp, n-1, bCount, cCount);
-    res += stringCountUtil(dp, n-1, bCount-1, cCount);
-    res += stringCountUtil(dp, n-1, bCount, cCount-1);
+    int counter = 0;
+    counter += dpFindCountUtil(dp, n-1, bCount, cCount);
+    counter += dpFindCountUtil(dp, n-1, bCount-1, cCount);
+    counter += dpFindCountUtil(dp, n-1, bCount, cCount-1);
 
-    return (dp[n][bCount][cCount]=res);
+    return dp[n][bCount][cCount] = counter;
 }
 
-int stringCount(int n, int bCount, int cCount){
+int dpFindCount(int n, int bCount, int cCount)
+{
     int dp[n+1][2][3];
     memset(dp, -1, sizeof(dp));
-    return stringCountUtil(dp, n, bCount, cCount);
+    return dpFindCountUtil(dp, n, bCount, cCount);
 }
+
 
 int main(){
     int noOfChars=3;
 
-    int count = findCount(noOfChars,1,2);
-    cout<<"String Count = "<<count<<endl;
+    int rcCount = findCount(noOfChars,1,2);
+    cout<<"String Count = "<<rcCount<<endl;
 
-    int mycount = stringCount(noOfChars,1,2);
-    cout<<"String Count = "<<mycount<<endl;
+    int dpCount = dpFindCount(noOfChars,1,2);
+    cout<<"String Count = "<<dpCount<<endl;
 
     return 0;
 }
 
+/************************
+Recursive Method:
+Time Complexity  : O(3^N)
+Space Complexity : O(1)
+
+Dynamic Programming
+Time Complexity  : O(N)
+Space Complexity : O(N)
+*************************/
