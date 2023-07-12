b67103edf07b64f9a80a695268e9032f62395232
ManishKrishnaSingh <30351569+ManishKrishnaSingh@users.noreply.github.com>
2023-07-12 13:36:16 +0530

    [mixed] find minimum cost tickets for set of travel days - II

--- mixed/FindMinimumCostTickets-II.cpp
+++ mixed/FindMinimumCostTickets-II.cpp
@@ -2,6 +2,22 @@
 
 using namespace std;
 
+/*******************************************************************************************
+You have planned some train traveling one year in advance. The days of the year in which you
+will travel are given as an integer array days. Each day is an integer from 1 to 365.
+
+Train tickets are sold in three different ways:
+	-  1-day pass is sold for costs[0] dollars
+	-  7-day pass is sold for costs[1] dollars
+	- 30-day pass is sold for costs[2] dollars
+
+The passes allow that many days of consecutive travel.
+
+For example, if we get a 7-day pass on day 2, then we can travel for 7 days: 2,3,4,5,6,7 & 8
+
+Return the minimum number of dollars you need to travel every day in the given list of days.
+********************************************************************************************/
+
 vector<int> durations = {1,7,30};
 
 int minCostUtil(int index, int dp[], vector<int>& days, vector<int>& costs){
