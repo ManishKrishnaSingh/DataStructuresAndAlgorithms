0b606135b683c55a765a5df4d9d459c6a146fd7c
ManishKrishnaSingh <30351569+ManishKrishnaSingh@users.noreply.github.com>
2023-07-12 13:34:39 +0530

    [mixed] find minimum cost tickets for set of travel days

--- mixed/FindMinimumCostTickets.cpp
+++ mixed/FindMinimumCostTickets.cpp
@@ -2,9 +2,26 @@
 
 using namespace std;
 
+/*******************************************************************************************
+You have planned some train traveling one year in advance. The days of the year in which you
+will travel are given as an integer array days. Each day is an integer from 1 to 365.
+
+Train tickets are sold in three different ways:
+	-  1-day pass is sold for costs[0] dollars
+	-  7-day pass is sold for costs[1] dollars
+	- 30-day pass is sold for costs[2] dollars
+
+The passes allow that many days of consecutive travel.
+
+For example, if we get a 7-day pass on day 2, then we can travel for 7 days: 2,3,4,5,6,7 & 8
+
+Return the minimum number of dollars you need to travel every day in the given list of days.
+********************************************************************************************/
+
 int minCostTickets(vector<int>& days, vector<int>& costs){
     unordered_set<int> hash;
-    for(int day : days){
+    for(int day : days)
+    {
         hash.insert(day);
     }
 
@@ -12,9 +29,11 @@ int minCostTickets(vector<int>& days, vector<int>& costs){
     int dp[lastday+1];
 
     dp[0] = 0; // base case
-    for(int i=1; i<=lastday; i++){
+    for(int i=1; i<=lastday; i++)
+    {
         // update prev day cost & cont.
-        if(hash.find(i) == hash.end()){
+        if(hash.find(i) == hash.end())
+        {
             dp[i] = dp[i-1]; continue;
         }
 
@@ -29,7 +48,8 @@ int minCostTickets(vector<int>& days, vector<int>& costs){
     return dp[lastday];
 }
 
-int main() {
+int main()
+{
     vector<int> cost = {2,7,15};
     vector<int> days = {1,4,6,7,8,20};
     
@@ -38,3 +58,8 @@ int main() {
 
     return 0;
 }
+
+/****************************
+Time Complexity  : O(LastDay)
+Space Complexity : O(LastDay)
+*****************************/
