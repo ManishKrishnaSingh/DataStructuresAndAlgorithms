3f162fc1c2d181d9c216d02f98c7f2e8c8f4f296
ManishKrishnaSingh <30351569+ManishKrishnaSingh@users.noreply.github.com>
2023-07-12 23:39:11 +0530

    [mixed] find maximum score from word break using dp

--- mixed/FindMaximumScoreWordBreakDP.cpp
+++ mixed/FindMaximumScoreWordBreakDP.cpp
@@ -2,76 +2,106 @@
 
 using namespace std;
 
+
 /** map contains pair <word, value> */
-unordered_map<string,int> dictionary={
+unordered_map<string,int> dictionary =
+{
     {"cats", 4}, {"cat", 5}, {"and", 1}, {"sand", 5}, {"dog", 8}
 };
 
-vector<string> split(string iStr){
+
+vector<string> split(string str)
+{
     vector<string> list;
+
     string line;
-    stringstream ss(iStr);
-    while(getline(ss, line, ' ')){
+    stringstream ss(str);
+    while(getline(ss, line, ' '))
+    {
         list.push_back(line);
     }
+
     return list;
 }
 
-bool IsValidWord(string iStr){
-    if(dictionary.find(iStr)!=dictionary.end()){
+bool IsValidWord(string str)
+{
+    if(dictionary.count(str))
+    {
         return true;
     }
     return false;
 }
 
-pair<string,int> maximumScoreWordBreak(string iStr){
-    vector<string> vec;
-    int n = iStr.length();
+
+pair<string,int> findMaximumScoreOnWordBreak(string str)
+{
+    int i, j, n=str.length();
 
     // store word with index
     vector<vector<string>> dp(n);
-    for(int i=0; i<n; i++){
-        vec.clear();
-        for(int j = i+1; j<n+1; j++){
-            if(IsValidWord(iStr.substr(i,j-i))){
-                vec.push_back(iStr.substr(i,j-i));
+
+    vector<string> words;
+    for(i = 0; i < n; i++)
+    {
+        words.clear();
+
+        for(j = i+1; j < n+1; j++)
+        {
+            if(IsValidWord(str.substr(i, j-i)))
+            {
+                words.push_back(str.substr(i, j-i));
             }
         }
-        dp[i] = vec;
+
+        dp[i] = words;
     }
+
 
     // bottom-up approch form solution   
     vector<vector<string>> solution(n);
-    for(int i=n-1; i>=0; i--){
-        vec.clear();
-        if (dp[i].size()) {
-            for(auto word : dp[i]){
+    for(i = n-1; i >= 0; i--)
+    {
+        words.clear();
+
+        for(auto& word : dp[i])
+        {
                 int wLen = word.length();
+
                 // if word is last word
-                if(i+wLen == n){
-                    vec.push_back(word);
-                } else if(solution[i+wLen].size()){
-                    for(auto wstr : solution[i+wLen]){
-                        vec.push_back(word+" "+wstr);
+                if(i+wLen == n)
+                {
+                    words.push_back(word);
+                }
+                else if(solution[i+wLen].size())
+                {
+                    for(auto& strWord : solution[i+wLen])
+                    {
+                        words.push_back(word + " " + strWord);
                     }
                 }
-            }
         }
-        solution[i] = vec;
+
+        solution[i] = words;
     }
 
-    // return solution
+
+    // return the solution
     string strSolution="";
-    int currScore,maxScore=0;
-    for(string line : solution[0]){
+    int currScore, maxScore=0;
+    for(string& line : solution[0])
+    {
         currScore = 0;
-        //calculate cost to form line
-        for(string word : split(line)){
-            currScore+=dictionary[word];
+
+        // calculate cost to form line
+        for(string& word : split(line))
+        {
+            currScore += dictionary[word];
         }
 
-        //update line & maxScore
-        if(currScore > maxScore){
+        // update line+maxScore
+        if(currScore > maxScore)
+        {
             strSolution = line;
             maxScore = currScore;
         }
@@ -80,10 +110,13 @@ pair<string,int> maximumScoreWordBreak(string iStr){
 	return {strSolution, maxScore};
 }
 
-int main(){
+
+int main()
+{
     string str = "catsanddog";
-    auto res = maximumScoreWordBreak(str);
-    cout<<"Score["<<res.first<<"]="<<res.second;
+
+    auto res = findMaximumScoreOnWordBreak(str);
+    cout<<"Score ["<<res.first<<"] = "<<res.second;
 
     return 0;
 }
