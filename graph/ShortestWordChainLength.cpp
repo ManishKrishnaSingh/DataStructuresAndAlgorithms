6de7b42cbc6aaab1571527dca64ec7c079ceaeca
ManishKrishnaSingh <30351569+ManishKrishnaSingh@users.noreply.github.com>
2023-07-14 11:48:56 +0530

    [graph] find shortest word chain length using priority queue

---
+++ graph/ShortestWordChainLength.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool IsDifferByOne(string x, string y)
+{
+    int count=0;
+    int n=min(x.size(),y.size());
+
+    for(int i=0; i < n; i++)
+    {
+        if(x[i] != y[i])
+        {
+            count++;
+        }
+    }
+
+    if(count == 1)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+void updateMapOfDictionaryData(vector<string>& dictionary, unordered_map<string,unordered_set<string>>& mapData)
+{
+    int i,j,n=dictionary.size();
+
+    for(i=0; i < n; i++)
+    {
+        for(j=i+1; j < n; j++)
+        {
+            if(IsDifferByOne(dictionary[i], dictionary[j]))
+            {
+                mapData[dictionary[i]].insert(dictionary[j]);
+                mapData[dictionary[j]].insert(dictionary[i]);
+            }
+        }
+    }
+}
+
+int shortestChainLength(vector<string>& dictionary,string start,string final)
+{
+    if(start == final)
+    {
+        return 0;
+    }
+
+    // add start and final
+    dictionary.push_back(start);
+    dictionary.push_back(final);
+
+    unordered_map<string,unordered_set<string>> mapData;
+    updateMapOfDictionaryData(dictionary, mapData);
+
+    unordered_set<string> visit;
+    priority_queue<pair<int,string>, vector<pair<int,string>>, greater<pair<int,string>>> pq;
+
+    pq.push({1, start});
+    while(!pq.empty())
+    {
+        auto top = pq.top(); pq.pop();
+
+        auto& dist = top.first;
+        auto& word = top.second;
+
+        if(word == final)
+        {
+            return dist;
+        }
+
+        visit.insert(word);
+
+        for(auto str : mapData[word])
+        {
+            if(visit.count(str) == 0)
+            {
+                pq.push({dist+1, str});
+            }
+        }
+    }
+
+    return 0;
+}
+
+int main(){
+    vector<string> dictionary =
+    {
+        "poon", "plee", "same", "poie", "plie", "poin", "plea"
+    };
+
+    int ladderLength = shortestChainLength(dictionary, "toon", "plea");
+    cout<<"Shortest Chain Length [Dictionary] = "<<ladderLength<<endl;
+
+    return 0;
+}
