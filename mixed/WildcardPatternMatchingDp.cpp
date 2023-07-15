893c456665399ded8c8dc6ce43d6a5695793d88f
ManishKrishnaSingh <30351569+ManishKrishnaSingh@users.noreply.github.com>
2023-07-15 22:37:53 +0530

    [mixed] wildcard pattern matching in string using dp

---
+++ mixed/WildcardPatternMatchingDp.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool matches(string str, string pat, int m, int n)
+{
+    if(m == 0 && n==0)
+    {
+        return true;
+    }
+
+    bool dp[m+1][n+1];
+    memset(dp, false, sizeof(dp));
+
+    dp[0][0] = true;
+
+    int i, j;
+
+    // '*' with empty string
+    for(j = 1; j <= n; j++)
+    {
+        if(pat[j-1] == '*')
+        {
+            dp[0][j] = dp[0][j-1];
+        }
+    }
+
+    // now, fill bottom-up
+    for(i = 1; i <= m; i++)
+    {
+        for(j = 1; j <= n; j++)
+        {
+            if (pat[j-1] == '*')
+            {
+                dp[i][j] = dp[i][j-1] || dp[i-1][j];
+            }
+            else if(pat[j-1]=='?' || str[i-1]==pat[j-1])
+            {
+                dp[i][j] = dp[i-1][j-1];
+            }
+            else
+            {
+                dp[i][j] = false;
+            }
+        }
+    }
+
+    return dp[m][n];
+}
+
+int main()
+{
+    string str = "baaabab";
+    string pat = "*****ba*****ab";
+
+    bool IsMatch = matches(str,pat,str.length(),pat.length());
+    cout<<"Found the regex match = "<<boolalpha<<IsMatch<<endl;
+
+    return 0;
+}
+
+/************************
+Time Complexity  : O(M*N)
+Space Complexity : O(M*N)
+*************************/
