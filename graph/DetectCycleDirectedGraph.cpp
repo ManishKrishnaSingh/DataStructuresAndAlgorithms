757b351d3ce843cb79f86db2b38913b9797b1663
ManishKrishnaSingh <30351569+ManishKrishnaSingh@users.noreply.github.com>
2023-07-30 01:08:29 +0530

    [graph] update hamiltonian cycle in the directed graph

--- graph/DetectCycleDirectedGraph.cpp
+++ graph/DetectCycleDirectedGraph.cpp
@@ -6,61 +6,67 @@ using namespace std;
 
 typedef pair<int,int> Edge;
 
-class Graph{
-  private:
+class Graph
+{
 	int V;
 	list<Edge> listOfEdge;
 
-  public:
-	Graph(int V){
+public:
+	Graph(int V)
+	{
 		this->V = V;
 	}
 
-	void addEdge(int src, int dst){
+	void addEdge(int src, int dst)
+	{
 		listOfEdge.push_back({src, dst});
 	}
 
-	int find(int parent[], int index){
-	    if(parent[index] == -1){
+	int find(int parent[], int index)
+	{
+	    if(parent[index] == -1)
+	    {
 	        return index;
 	    }
 	    return find(parent, parent[index]);
 	}
 
-	void Union(int parent[], int u, int v){
+	void Union(int parent[], int u, int v)
+	{
 	    parent[u] = v;
 	}
 
-	bool hasCycle(){
+	bool hasCycle()
+	{
 	    int parent[this->V];
 	    memset(parent, -1, sizeof(parent));
 
 	    list<Edge>::iterator it;
-	    for(it=listOfEdge.begin(); it!=listOfEdge.end(); it++){
+	    for(it=listOfEdge.begin(); it!=listOfEdge.end(); it++)
+	    {
 	        int x = find(parent, it->first);
 	        int y = find(parent, it->second);
 
-	        if(x==y){
+	        if(x == y)
+	        {
 	            return true;
 	        }
+
 	        Union(parent, x, y);
 	    }
 	    return false;
 	}
 };
 
+int main()
+{
+	Graph graph(3);
 
+	graph.addEdge(0, 1);
+	graph.addEdge(0, 2);
+	graph.addEdge(1, 2);
 
-int main(){
-	int V = 3;
-	Graph* graph = new Graph(V);
-
-	graph->addEdge(0, 1);
-	graph->addEdge(0, 2);
-	graph->addEdge(1, 2);
-
-	cout<<"Graph has Cycle = "<<boolalpha<<graph->hasCycle()<<endl;
+	cout<<"Graph has Cycle = "<<boolalpha<<graph.hasCycle();
 
 	return 0;
 }
-
