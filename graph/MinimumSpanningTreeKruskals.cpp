87888e59bea5e7518070e2ce6172371a11232e56
ManishKrishnaSingh <30351569+ManishKrishnaSingh@users.noreply.github.com>
2023-08-02 01:58:37 +0530

    [graph] minimum spanning tree using kruskals algorithm

--- graph/MinimumSpanningTreeKruskals.cpp
+++ graph/MinimumSpanningTreeKruskals.cpp
@@ -2,103 +2,129 @@
 
 using namespace std;
 
-class DisjointSetUnion {
+class DisjointSetUnion
+{
     int* rank;
     int* parent;
 
-  public:
-    DisjointSetUnion(int n){
+public:
+    DisjointSetUnion(int n)
+    {
         rank = new int[n];
         parent = new int[n];
-        for(int i=0; i<n; i++){
+        for(int i=0; i<n; i++)
+        {
             rank[i] = i;
             parent[i] = -1;
         }
     }
 
-    int find(int x){
-        if(parent[x] == -1){
+    int find(int x)
+    {
+        if(parent[x] == -1)
+        {
             return x;
         }
         return parent[x] = find(parent[x]);
     }
 
-    void Union(int x, int y){
+    void Union(int x, int y)
+    {
         int pX = find(x);
         int pY = find(y);
 
-        if(pX == pY){
+        if(pX == pY)
+        {
             return;
         }
 
-        if(rank[pX] < rank[pY]){
+        if(rank[pX] < rank[pY])
+        {
             parent[pX] = pY;
-        } else if(rank[pX] > rank[pY]){
+        }
+        else if(rank[pX] > rank[pY])
+        {
             parent[pY] = pX;
-        } else {
+        }
+        else
+        {
             parent[pY] = pX;
             rank[pX] = rank[pX] + 1;
         }
     }
 };
 
-typedef struct edge{
+struct Edge
+{
     int start, finish, weight;
-    edge(int u, int v, int w){
+    Edge(int u, int v, int w)
+    {
         this->start  = u;
         this->finish = v;
         this->weight = w;
     }
-}Edge;
+};
 
-bool compare(Edge e1, Edge e2){
-    if(e1.start != e2.start){
+bool compare(const Edge& e1, const Edge& e2)
+{
+    if(e1.start != e2.start)
+    {
         return e1.start < e2.start;
-    } else if(e1.finish != e2.finish){
+    }
+    else if(e1.finish != e2.finish)
+    {
         return e1.finish < e2.finish;
     }
     return e1.weight < e2.weight;
 }
 
-class Graph{
+class Graph
+{
     int V;
-    vector<Edge> edges;
+    vector<Edge> vEdge;
 
-  public:
-    Graph(int V){
-        this->V = V;
-    }
-
-    ~Graph(){
+public:
+    Graph(int iV):V(iV)
+    {
         //TODO
     }
 
-    void addEdge(int u, int v, int w){
-        edges.emplace_back(u,v,w);
+    ~Graph()
+    {
+        //TODO
     }
 
-    int findMinimumCost(){
-        sort(edges.begin(), edges.end(), compare);
+    void addEdge(int u, int v, int w)
+    {
+        vEdge.emplace_back(u,v,w);
+    }
 
-        int minCost = 0;
+    int findMinimumCost()
+    {
+        sort(vEdge.begin(), vEdge.end(), compare);
+
+        int minimumCost = 0;
         DisjointSetUnion dsu(V);
 
-        for(Edge edge :  edges){
-            int u = edge.start;
-            int v = edge.finish;
-            int w = edge.weight;
-            if(dsu.find(u) != dsu.find(v)){
+        for(Edge edge :  vEdge)
+        {
+            int& u = edge.start;
+            int& v = edge.finish;
+            int& w = edge.weight;
+            if(dsu.find(u) != dsu.find(v))
+            {
                 dsu.Union(u, v);
-                minCost = minCost + w;
-                cout<<"Edge : "<<u<<" - "<<v<<endl;
+                minimumCost = minimumCost+w;
+                cout<<"Edge: "<<u<<" - "<<v<<"\n";
             }
         }
 
-        return minCost;
+        return minimumCost;
     }
 };
 
-int main(){
+int main()
+{
     Graph graph(4);
 
     graph.addEdge(0, 1, 1);
