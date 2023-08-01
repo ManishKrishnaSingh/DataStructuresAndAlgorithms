5be7522a3ecad73e25a02259907e792fcb0c7d01
ManishKrishnaSingh <30351569+ManishKrishnaSingh@users.noreply.github.com>
2023-08-02 02:24:05 +0530

    [graph] add continue if node already visited in prims algo

--- graph/MinimumSpanningTreePrimsPQ.cpp
+++ graph/MinimumSpanningTreePrimsPQ.cpp
@@ -2,51 +2,55 @@
 
 using namespace std;
 
-typedef pair<int,int> Node;
-
-class Graph{
+class Graph
+{
     int V;
     list<pair<int,int>> *adj;
 
-  public:
-    Graph(int V){
-        this->V = V;
-        this->adj = new list<pair<int,int>>[V];
+public:
+    Graph(int iV):V(iV)
+    {
+        adj = new list<pair<int,int>>[V];
     }
 
-    ~Graph(){
+    ~Graph()
+    {
         delete[] adj;
     }
 
-    void addEdge(int u, int v, int w){
-        adj[u].push_back(make_pair(v,w));
-        adj[v].push_back(make_pair(u,w));
+    void addEdge(int u, int v, int w)
+    {
+        adj[u].push_back({v,w});
+        adj[v].push_back({u,w});
     }
 
-    void minSpanningTree(){
-        priority_queue<Node,vector<Node>,greater<Node>> pq;
+    void minSpanningTree()
+    {
+        vector<bool> visited(V, false);
+        vector<int>  parent(V, -1);
+        vector<int>  weight(V, INT_MAX);
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
 
-        vector<int> parent(V, -1);
-        vector<int> weight(V, INT_MAX);
+        pq.push({0, 0}); // start with vertex "0"
 
-        vector<bool> visit(V, false);
-
-        int start = 0;
-        pq.push({0, start});
-        while(!pq.empty()){
+        while(!pq.empty())
+        {
             int u=pq.top().second; pq.pop();
 
-            if(visit[u])
+            if(visited[u])
             {
                 continue;
             }
 
-            visit[u] = true;
+            visited[u] = true;
 
-            for(auto iter=adj[u].begin(); iter!=adj[u].end(); iter++){
-                int v = iter->first;
-                int w = iter->second;
-                if (!visit[v] and weight[v] > w){
+            for(auto& pEntry : adj[u])
+            {
+                int& v = pEntry.first;
+                int& w = pEntry.second;
+
+                if (!visited[v] and weight[v] > w)
+                {
                     parent[v] = u;
                     weight[v] = w;
                     pq.push({weight[v], v});
@@ -54,13 +58,15 @@ class Graph{
             }
         }
 
-        for (int v=1; v<V; v++){
+        for (int v = 1; v < V; v++)
+        {
             cout<<"Edge: "<<parent[v]<<" - "<<v<<endl;
         }
     }
 };
 
-int main(){
+int main()
+{
     Graph graph(9);
 
     graph.addEdge(0, 1, 4);
